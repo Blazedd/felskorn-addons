6d899a1325a1f255d6faf2fa08b349113c9e830c
Hawjiki <zelos160@gmail.com>
2024-10-14 15:26:29 -0500

    Atlasloot

---
+++ AtlasLoot_Felskorn/AtlasLoot_Felskorn/felskorn.lua
@@ -0,0 +1,232 @@
+local AL = LibStub("AceLocale-3.0"):GetLocale("AtlasLoot");
+local BabbleBoss = AtlasLoot_GetLocaleLibBabble("LibBabble-Boss-3.0")
+local BabbleInventory = AtlasLoot_GetLocaleLibBabble("LibBabble-Inventory-3.0")
+local BabbleFaction = AtlasLoot_GetLocaleLibBabble("LibBabble-Faction-3.0")
+local BabbleZone = AtlasLoot_GetLocaleLibBabble("LibBabble-Zone-3.0")
+
+
+-- Index
+--- Dungeons & Raids
+---- Scarlet Monastery HC
+
+
+	------------------------
+	--- Dungeons & Raids ---
+	------------------------
+
+		----------------------------
+		--- Scarlet Monastery HC ---
+		----------------------------
+	AtlasLoot_Data["SMHVishas"] = {
+{1, 4000134, "", "Scarlet Plate Helm", "", "", ""};
+{2, 4000032, "", "Scarlet Ring", "", "", ""};
+{3, 4000033, "", "Scralet Mail Wrists", "", "", ""};
+{4, 4000126, "", "Scarlet Plate Pants", "", "", ""};
+{5, 4000082, "", "Scarlet Scalpel", "", "", ""};
+{6, 4000117, "", "Scralet Plate Bindings", "", "", ""};
+{7, 4000025, "", "Scralet Mail Crown", "", "", ""};
+{8, 4000131, "", "Scarlet Beads", "", "", ""};
+{9, 4000003, "", "Scarlet Cloth Cord", "", "", ""};
+{10, 4000053, "", "Scarlet Greatstaff", "", "", ""};
+{11, 4000068, "", "Scarlet Binding", "", "", ""};
+{12, 4000087, "", "Scarlet Cloth Boots", "", "", ""};
+{13, 4000118, "", "Scarlet Maul", "", "", ""};
+{14, 4000099, "", "Scarlet Focus", "", "", ""};
+{15, 4000004, "", "Scarlet Plate Boots", "", "", ""};
+{16, 4000098, "", "Scarlet Cloth Cowl", "", "", ""};
+{20, 4000000, "", "Scarlet Prophet's Bauble", "", "", "25%"};
+{21, 4000012, "", "Scarlet Militant's Emblem", "", "", "25%"};
+
+		Next = "SMHThalnos";
+	};
+	
+		AtlasLoot_Data["SMHThalnos"] = {
+{1, 4000047, "", "Scarlet Greataxe", "", "", ""};
+{2, 4000020, "", "Scarlet Plate Helm", "", "", ""};
+{3, 4000043, "", "Scralet Plate Wrists", "", "", ""};
+{4, 4000119, "", "Scarlet Carbine", "", "", ""};
+{5, 4000017, "", "Scarlet Cloth Handshrouds", "", "", ""};
+{6, 4000073, "", "Scarlet Cloth Mantle", "", "", ""};
+{7, 4000051, "", "Scralet Leather Wrists", "", "", ""};
+{8, 4000095, "", "Scarlet Longsword", "", "", ""};
+{9, 4000056, "", "Scarlet Collar", "", "", ""};
+{10, 4000062, "", "Scralet Cloth Wrists", "", "", ""};
+{11, 4000054, "", "Scarlet Scarf", "", "", ""};
+{12, 4000026, "", "Scralet Plate Bracers", "", "", ""};
+{13, 4000100, "", "Scarlet Leather Shoulderpads", "", "", ""};
+{14, 4000094, "", "Scarlet Greatspear", "Polearm", "", ""};
+{15, 4000058, "", "Scarlet Plate Fingerguards", "", "", ""};
+{16, 4000115, "", "Scarlet Leather Shoulders", "", "", ""};
+{20, 4000024, "", "Scarlet Spellbinder's Sigil", "", "", "25%"};
+{21, 4000072, "", "Scarlet Champion's Badge", "", "", "25%"};
+		
+		Prev = "SMHVishas";
+		Next = "SMHLoksey";
+	};
+	
+		AtlasLoot_Data["SMHLoksey"] = {
+{1, 4000052, "", "Scarlet Plate Gloves", "", "", ""};
+{2, 4000008, "", "Scarlet Mail Belt", "", "", ""};
+{3, 4000035, "", "Scarlet Cleaver", "", "", ""};
+{4, 4000046, "", "Scarlet Leather Stranglers", "", "", ""};
+{5, 4000063, "", "Scarlet Mail Shoes", "", "", ""};
+{6, 4000080, "", "Scarlet Leather Mask", "", "", ""};
+{7, 4000071, "", "Scarlet Wild Staff", "", "", ""};
+{8, 4000091, "", "Scarlet Plate Legguards", "", "", ""};
+{9, 4000006, "", "Scarlet Silk Shawl", "", "", ""};
+{10, 4000125, "", "Scarlet Plate Stompers", "", "", ""};
+{11, 4000010, "", "Scarlet Scepter", "", "", ""};
+{12, 4000096, "", "Scarlet Plate Gauntlets", "", "", ""};
+{13, 4000048, "", "Scarlet Cloth Kilt", "", "", ""};
+{14, 4000002, "", "Scralet Mail Bracers", "", "", ""};
+{15, 4000036, "", "Scarlet Pendant", "", "", ""};
+{16, 4000111, "", "Scarlet Wrappings", "", "", ""};
+{20, 4000138, "", "Scarlet Pugilist's Medallion", "", "", "25%"};
+{21, 4000139, "", "Scarlet Defender's Sigil", "", "", "25%"};
+	
+	Prev = "SMHThalnos";		
+	Next = "SMHDoan";
+	};
+	
+		AtlasLoot_Data["SMHDoan"] = {
+{1, 4000112, "", "Scarlet Mail Gloves", "", "", ""};
+{2, 4000077, "", "Scarlet Amulet", "", "", ""};
+{3, 4000013, "", "Scarlet Plate Cuirass", "", "", ""};
+{4, 4000135, "", "Scarlet Plate Helm", "", "", ""};
+{5, 4000042, "", "Scarlet Band", "", "", ""};
+{6, 4000028, "", "Scarlet Redoubt", "", "", ""};
+{7, 4000031, "", "Scarlet Cloak", "", "", ""};
+{8, 4000083, "", "Scarlet Sword", "", "", ""};
+{9, 4000081, "", "Scarlet Necklet", "", "", ""};
+{10, 4000089, "", "Scarlet Wand", "", "", ""};
+{11, 4000105, "", "Scarlet Plate Handguards", "", "", ""};
+{12, 4000123, "", "Scarlet Leather Shapers", "", "", ""};
+{13, 4000069, "", "Scarlet Plate Strap", "", "", ""};
+{14, 4000120, "", "Scarlet Mail Shoulderguards", "", "", ""};
+{15, 4000005, "", "Scarlet Spellblade", "", "", ""};
+{16, 4000074, "", "Scarlet Mail Kilt", "", "", ""};
+{20, 4000140, "", "Scarlet Mystic's Trinket", "", "", "25%"};
+{21, 4000141, "", "Scarlet Spiritualist's Emblem", "", "", "25%"};
+	
+	Prev = "SMHLoksey";
+	Next = "SMHBilly";
+	};
+	
+		AtlasLoot_Data["SMHBilly"] = {
+{1, 4000122, "", "Scarlet Cloth Robe", "", "", ""};
+{2, 4000011, "", "Scarlet Splitter", "", "", ""};
+{3, 4000101, "", "Scarlet Mail Chestguard", "", "", ""};
+{4, 4000136, "", "Scarlet Carbine", "", "", ""};
+{5, 4000022, "", "Scarlet Mail Handgrips", "", "", ""};
+{6, 4000044, "", "Scarlet Plate Carapace", "", "", ""};
+{7, 4000128, "", "Scarlet Left Fist", "", "", ""};
+{8, 4000041, "", "Scarlet Cloth Sandals", "", "", ""};
+{9, 4000129, "", "Scralet Leather Vambraces", "", "", ""};
+{10, 4000088, "", "Scarlet Cloth Raiment", "", "", ""};
+{11, 4000038, "", "Scarlet Cloth Tiara", "", "", ""};
+{12, 4000114, "", "Scarlet Mail Legguards", "", "", ""};
+{13, 4000039, "", "Scarlet Leather Pants", "", "", ""};
+{14, 4000014, "", "Scralet Cloth Bracers", "", "", ""};
+{15, 4000127, "", "Scarlet Mail Treads", "", "", ""};
+{16, 4000078, "", "Scarlet Mail Shoulderpads", "", "", ""};
+{20, 4000142, "", "Scarlet Theurgist's Medallion", "", "", "25%"};
+{21, 4000143, "", "Scarlet Panzer's Emblem", "", "", "25%"};
+	
+	Prev = "SMHDoan";
+	Next = "SMHHerod";
+	};
+	
+		AtlasLoot_Data["SMHHerod"] = {
+{1, 4000023, "", "Scarlet Longbow", "Bow", "", ""};
+{2, 4000106, "", "Scarlet Crossbow", "Crossbow", "", ""};
+{3, 4000030, "", "Scarlet Mail Pauldrons", "", "", ""};
+{4, 4000102, "", "Scarlet Leather Leggings", "", "", ""};
+{5, 4000110, "", "Scarlet Leather Hood", "", "", ""};
+{6, 4000067, "", "Scarlet Mail Cap", "", "", ""};
+{7, 4000108, "", "Scarlet Cloth Leggings", "", "", ""};
+{8, 4000093, "", "Scarlet Leather Slippers", "", "", ""};
+{9, 4000040, "", "Scarlet Staff", "", "", ""};
+{10, 4000079, "", "Scarlet Plate Shoulderguards", "", "", ""};
+{11, 4000027, "", "Scarlet Leather Cord", "", "", ""};
+{12, 4000055, "", "Scarlet Leather Trousers", "", "", ""};
+{13, 4000015, "", "Scarlet Mail Cord", "", "", ""};
+{14, 4000016, "", "Scarlet Leather Boots", "", "", ""};
+{15, 4000007, "", "Scarlet Choker", "", "", ""};
+{16, 4000070, "", "Scarlet Knife", "", "", ""};
+{20, 4000144, "", "Scarlet Priest's Sigil", "", "", "25%"};
+{21, 4000145, "", "Scarlet Combatant's Emblem", "", "", "25%"};
+		
+	Prev = "SMHBilly";
+	Next = "SMHMograine";
+	};
+	
+		AtlasLoot_Data["SMHMograine"] = {
+{1, 4000086, "", "Scarlet Circle", "", "", ""};
+{2, 4000146, "", "Scarlet Shotgun Longbow", "", "", ""};
+{3, 4000034, "", "Scarlet Plate Treads", "", "", ""};
+{4, 4000061, "", "Scarlet Locket", "", "", ""};
+{5, 4000021, "", "Scarlet Leather Belt", "", "", ""};
+{6, 4000133, "", "Scarlet Redoubt", "", "", ""};
+{7, 4000075, "", "Scarlet Plate Chestplate", "", "", ""};
+{8, 4000060, "", "Scarlet Leather Robe", "", "", ""};
+{9, 4000001, "", "Scarlet Plate Leggings", "", "", ""};
+{10, 4000045, "", "Scarlet Mail Hauberk", "", "", ""};
+{11, 4000124, "", "Scarlet Carver", "", "", ""};
+{12, 4000109, "", "Scarlet Plate Pauldrons", "", "", ""};
+{13, 4000085, "", "Scarlet Cape", "", "", ""};
+{14, 4000009, "", "Scarlet Claymore", "", "", ""};
+{15, 4000037, "", "Scarlet Plate Legplates", "", "", ""};		
+{20, 4000000, "", "Scarlet Prophet's Bauble", "", "", "25%"};
+{21, 4000012, "", "Scarlet Militant's Emblem", "", "", "25%"};
+{22, 4000024, "", "Scarlet Spellbinder's Sigil", "", "", "25%"};
+{23, 4000072, "", "Scarlet Champion's Badge", "", "", "25%"};
+{24, 4000138, "", "Scarlet Pugilist's Medallion", "", "", "25%"};
+{25, 4000139, "", "Scarlet Defender's Sigil", "", "", "25%"};
+
+	Prev = "SMHHerod";
+	Next = "SMHWhitemane";
+	};
+	
+		AtlasLoot_Data["SMHWhitemane"] = {
+{1, 4000132, "", "Scarlet Bulwark", "", "", ""};
+{2, 4000116, "", "Scarlet Plate Belt", "", "", ""};
+{3, 4000084, "", "Scarlet Plate Spaulders", "", "", ""};
+{4, 4000107, "", "Scarlet Needle", "", "", ""};
+{5, 4000076, "", "Scarlet Cloth Wrap", "", "", ""};
+{6, 4000065, "", "Scarlet Hammer", "", "", ""};
+{7, 4000050, "", "Scarlet Signet", "", "", ""};
+{8, 4000130, "", "Scarlet Plate Girdle", "", "", ""};
+{9, 4000029, "", "Scarlet Scalpel", "", "", ""};
+{10, 4000018, "", "Scarlet Frill", "", "", ""};
+{11, 4000090, "", "Scarlet Mail Girdle", "", "", ""};
+{12, 4000064, "", "Scarlet Cloth Gloves", "", "", ""};
+{13, 4000049, "", "Scarlet Cloth Shoulderguards", "", "", ""};
+{14, 4000137, "", "Scarlet Plate Mits", "", "", ""};
+{15, 4000059, "", "Scarlet Gavel", "", "", ""};
+{16, 4000092, "", "Scarlet Hoop", "", "", ""};
+{20, 4000140, "", "Scarlet Mystic's Trinket", "", "", "25%"};
+{21, 4000141, "", "Scarlet Spiritualist's Emblem", "", "", "25%"};
+{22, 4000142, "", "Scarlet Theurgist's Medallion", "", "", "25%"};
+{23, 4000143, "", "Scarlet Panzer's Emblem", "", "", "25%"};
+{24, 4000144, "", "Scarlet Priest's Sigil", "", "", "25%"};
+{25, 4000145, "", "Scarlet Combatant's Emblem", "", "", "25%"};
+		
+	Prev = "SMHMograine";
+	Next = "SMHTrash";
+	};
+	
+		AtlasLoot_Data["SMHTrash"] = {
+{1, 4000019, "", "Scarlet Leather Chestguard", "", "", "5%"};
+{2, 4000113, "", "Scarlet Stick", "", "", "5%"};
+{3, 4000057, "", "Scarlet Mail Boots", "", "", "5%"};
+{4, 4000098, "", "Scarlet Cloth Cowl", "", "", "5%"};
+{5, 4000115, "", "Scarlet Leather Shoulders", "", "", "5%"};
+{6, 4000111, "", "Scarlet Wrappings", "", "", "5%"};
+{7, 4000074, "", "Scarlet Mail Kilt", "", "", "5%"};
+{8, 4000066, "", "Scarlet Right Fist", "", "", "5%"};
+{9, 4000097, "", "Scarlet Plate Waistband", "", "", "5%"};
+{10, 4000104, "", "Scarlet Seal", "", "", "5%"};
+{11, 4000103, "", "Scarlet Necklace", "", "", "5%"};
+
+	Prev = "SMHWhitemane";
+	};
